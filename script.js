9165e36291f75a1c4b45ddf3ae8583c573090b0b
Nisha Brijesh <92055350+nbhawnani@users.noreply.github.com>
2022-05-04 13:46:17 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+// 1st Way using new Object() method:
+let myCar = new Object();
+myCar.make = 'Ford';
+myCar.model = 'Mustang';
+myCar.year = 1969;
+
+
+//console.log(myCar)
+
+
+// 2nd Way: By adding properties and values in curly braces
+const myAddress = {
+streetnumber: 23,
+street: 'Red Street',
+city: 'Princeton',
+getLocation:function(){
+  console.log(myAddress.streetnumber + " " +myAddress.street + "," + myAddress.city )
+}
+};
+
+// Accessing object properties via "." and via "[]" methods:
+//console.log(myAddress,myAddress.street, myAddress["city"] )
+
+//Invoking the method inside the object
+//myAddress.getLocation();
+
+
+// Using for ... in Loop to access the property/method names and retrieving  the property/method values using dot or square notation:
+
+for (const property in myAddress){
+console.log(property + ':' + myAddress[property]);
+}
